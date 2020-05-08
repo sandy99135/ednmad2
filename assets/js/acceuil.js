56f83503a18b15065aa023660d4c0871a2df821b
sandy99135 <47249749+sandy99135@users.noreply.github.com>
2020-05-08 14:30:43 +0200

    Update acceuil.js

--- assets/js/acceuil.js
+++ assets/js/acceuil.js
@@ -1,2 +1,8 @@
 var slogan1=document.querySelector(".slogan1");
 var slogan2=document.querySelector(".slogan2");
+fetch('https://edn25.herokuapp.com/slogan').then(user=>user.json()).then(
+  function(data){
+    slogan1.innerHTML=data[data.length-1].slogan1;
+    slogan2.innerHTML=data[data.length-1].slogan2
+  }
+)
