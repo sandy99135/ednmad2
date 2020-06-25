92dd76a5efdf3562a3fb45b2ef5435f3a0599b8e
sandy99135 <47249749+sandy99135@users.noreply.github.com>
2020-06-25 15:44:04 +0200

    Update acceuil.js

--- assets/js/acceuil.js
+++ assets/js/acceuil.js
@@ -6,3 +6,20 @@ fetch('https://edn25.herokuapp.com/slogan').then(user=>user.json()).then(
     slogan2.innerHTML=data[data.length-1].slogan2
   }
 )
+fetch('https://edn25.herokuapp.com/text').then(user=>user.json()).then(
+  function(data){
+    text1.innerHTML=data[data.length-1].text1;
+    text2.innerHTML=data[data.length-1].text2
+    text3.innerHTML=data[data.length-1].text3
+  }
+)
+fetch('https://edn25.herokuapp.com/apropos').then(user=>user.json()).then(
+  function(data){
+    apropos1.innerHTML=data[data.length-1].apropos1;
+    apropos2.innerHTML=data[data.length-1].apropos2
+    apropos3.innerHTML=data[data.length-1].apropos3
+    apropos4.innerHTML=data[data.length-1].apropos4;
+    apropos5.innerHTML=data[data.length-1].apropos5
+    apropos6.innerHTML=data[data.length-1].apropos6
+  }
+)
